e6d74c56906ea45865f109316b90c262e39335a7
MithilreddyV <84663273+MithilreddyV@users.noreply.github.com>
2021-06-03 07:55:53 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,23 +4,15 @@
 #include <math.h>
 
 float wallis_pi(int);
-
-int main(void) {
-  float pi;
-  for (int i=0; i<5; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
-      abort();
-    }
-  }
-
-  for (int i=500; i<3000; i++) {
-    pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-      abort();
-    }
-  }
+int main(void){
+  int a;
+  a=get_int("Enter the value of looping constraint: ");
+  wallis_pi(a);
 }
-
+float wallis_pi(int a) {
+  float pi =1;
+  for(int i=1;i<=a;i++)
+    pi *= 4*pow(i,2)/(4*pow(i,2)-1)  ;
+  printf('"value of pi is %f",pi);
+}
+  
